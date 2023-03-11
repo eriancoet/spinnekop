d8a55a0316005e44ff3278e29c0e0b4da5888c1d
Rian Coetzee <eriancoet@gmail.com>
2023-03-11 16:40:09 +0200

    faith Loader

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/* 
+ihatetomatos loading screen code, open source, create a new color base and graphic of spider.
+*/
+
+$(document).ready(function() {
+	
+	setTimeout(function(){
+		$('body').addClass('loaded');
+		$('h1').css('color','#222222');
+	}, 1500);
+	
+});
